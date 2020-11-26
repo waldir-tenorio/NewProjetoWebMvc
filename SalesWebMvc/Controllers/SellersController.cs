0a749cf85edf1d086503bd008a0ffc6262d8c04c
Waldir Tenorio <waldir.tenorio@gmail.com>
2020-11-26 08:38:49 -0300

    Simple Create Form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -20,6 +21,19 @@ namespace SalesWebMvc.Controllers
         {
             var list = await _sellerServices.FindAllAsync();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Seller seller)
+        {
+            await _sellerServices.InsertAsync(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
