731474fe0c83c2865cf99239f4ca5854401bbab5
Waldir Tenorio <waldir.tenorio@gmail.com>
2020-12-13 21:47:33 -0300

    Update seller and custom service exception

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -2,6 +2,8 @@
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
+using SalesWebMvc.Services.Exceptions;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SalesWebMvc.Controllers
@@ -9,12 +11,12 @@ namespace SalesWebMvc.Controllers
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
-        private readonly DepartmentService _DepartmentService;
+        private readonly DepartmentService _departmentService;
 
         public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
-            _DepartmentService = departmentService;
+            _departmentService = departmentService;
         }
 
         public async Task<IActionResult> Index()
@@ -22,14 +24,19 @@ namespace SalesWebMvc.Controllers
             var list = await _sellerService.FindAllAsync();
             return View(list);
         }
-
+        /// <summary>
+        /// Create GET
+        /// </summary>
         public async Task<IActionResult> Create()
         {
-            var departments = await _DepartmentService.FindAllAsync();
+            var departments = await _departmentService.FindAllAsync();
             var viewModel = new SellerFormViewModel { Departments = departments };
             return View(viewModel);
         }
-
+        /// <summary>
+        /// Create POST
+        /// </summary>
+        /// <param name="seller"></param>
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Seller seller)
@@ -81,6 +88,53 @@ namespace SalesWebMvc.Controllers
                 return NotFound();
             }
             return View(obj);
+        }
+        /// <summary>
+        /// Edit GET
+        /// </summary>
+        /// <param name="id"></param>
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = await _sellerService.FindByIdAsync(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = await _departmentService.FindAllAsync();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+        /// <summary>
+        /// Edit POST
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="seller"></param>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                await _sellerService.UpdateAsync(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
